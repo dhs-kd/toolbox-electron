c1a18588f0672a290a2140cbd5d7ba18f0f54372
Momen Alany <41761574+0ud@users.noreply.github.com>
2022-03-27 17:05:35 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const { app, BrowserWindow } = require("electron");
+
+app.whenReady().then(createWindow);
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1366,
+    height: 768,
+    resizable: true,
+  });
+  win.loadFile("index.html");
+}
